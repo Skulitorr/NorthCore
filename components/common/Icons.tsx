7f985f70b226255e91cf9e21fe5b233dc48293d6
Skuli <Skulitor22@gmail.com>
2025-06-08 19:46:28 +0000

    Fix: added missing CalendarIcon and others in Icons.tsx

--- components/common/Icons.tsx
+++ components/common/Icons.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaCalendarAlt, FaEdit, FaUser, FaExclamationTriangle } from 'react-icons/fa';
 
 // This component provides a consistent set of icons used throughout the application
 // We're using simple SVG icons to avoid external dependencies
@@ -15,6 +16,23 @@ export interface IconsType {
 }
 
 const Icons: IconsType = {
+  // React Icons
+  CalendarAlt: ({ className, onClick }: IconProps) => (
+    <FaCalendarAlt className={className} onClick={onClick} />
+  ),
+  
+  Edit: ({ className, onClick }: IconProps) => (
+    <FaEdit className={className} onClick={onClick} />
+  ),
+  
+  User: ({ className, onClick }: IconProps) => (
+    <FaUser className={className} onClick={onClick} />
+  ),
+  
+  AlertTriangle: ({ className, onClick }: IconProps) => (
+    <FaExclamationTriangle className={className} onClick={onClick} />
+  ),
+  
   // Navigation icons
   Home: ({ className, onClick }: IconProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" onClick={onClick}>
@@ -37,13 +55,6 @@ const Icons: IconsType = {
       <line x1="16" y1="2" x2="16" y2="6"></line>
       <line x1="8" y1="2" x2="8" y2="6"></line>
       <line x1="3" y1="10" x2="21" y2="10"></line>
-    </svg>
-  ),
-  
-  User: ({ className, onClick }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" onClick={onClick}>
-      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-      <circle cx="12" cy="7" r="4"></circle>
     </svg>
   ),
   
@@ -90,13 +101,6 @@ const Icons: IconsType = {
     <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" onClick={onClick}>
       <line x1="12" y1="5" x2="12" y2="19"></line>
       <line x1="5" y1="12" x2="19" y2="12"></line>
-    </svg>
-  ),
-  
-  Edit: ({ className, onClick }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" onClick={onClick}>
-      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
     </svg>
   ),
   
