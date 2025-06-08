96f062e1ca3c11a9f09fb0c9aed7f3b0aa52905e
Skuli <Skulitor22@gmail.com>
2025-06-08 20:10:22 +0000

    fix: add missing icon exports for all views

--- components/common/Icons.tsx
+++ components/common/Icons.tsx
@@ -1,39 +1,38 @@
-import { FaCalendarAlt, FaEdit, FaUser, FaExclamationTriangle } from 'react-icons/fa';
-import { Map, Plus, Clock } from 'lucide-react';
-import React from 'react';
+import {
+  X,
+  Map,
+  BarChart3,
+  Calendar,
+  AlertTriangle,
+  Clock,
+  User,
+  Plus,
+  Edit,
+  Brain
+} from 'lucide-react';
 
-export const CalendarIcon = FaCalendarAlt;
-export const EditIcon = FaEdit;
-export const UserIcon = FaUser;
-export const AlertIcon = FaExclamationTriangle;
+export const XIcon = X;
 export const MapIcon = Map;
-export const PlusIcon = Plus;
+export const ChartIcon = BarChart3;
+export const CalendarIcon = Calendar;
+export const AlertIcon = AlertTriangle;
 export const ClockIcon = Clock;
-
-export const X = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor">
-    <line x1="18" y1="6" x2="6" y2="18" />
-    <line x1="6" y1="6" x2="18" y2="18" />
-  </svg>
-);
-
-export const Brain = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M9.5 2A2.5 2.5 0 0 1 12 4.5v15a2.5 2.5 0 0 1-4.96.44 2.5 2.5 0 0 1-2.96-3.08 3 3 0 0 1-.34-5.58 2.5 2.5 0 0 1 1.32-4.24 2.5 2.5 0 0 1 1.98-3A2.5 2.5 0 0 1 9.5 2Z" />
-    <path d="M14.5 2A2.5 2.5 0 0 0 12 4.5v15a2.5 2.5 0 0 0 4.96.44 2.5 2.5 0 0 0 2.96-3.08 3 3 0 0 0 .34-5.58 2.5 2.5 0 0 0-1.32-4.24 2.5 2.5 0 0 0-1.98-3A2.5 2.5 0 0 0 14.5 2Z" />
-  </svg>
-);
+export const UserIcon = User;
+export const PlusIcon = Plus;
+export const EditIcon = Edit;
+export const BrainIcon = Brain;
 
 const Icons = {
-  X,
-  Brain,
-  AlertIcon,
-  CalendarIcon,
-  EditIcon,
-  UserIcon,
+  XIcon,
   MapIcon,
-  PlusIcon,
+  ChartIcon,
+  CalendarIcon,
+  AlertIcon,
   ClockIcon,
+  UserIcon,
+  PlusIcon,
+  EditIcon,
+  BrainIcon,
 };
 
 export default Icons; 
